77db744f8f38c62f4eec2d256573117fa45ea476
SindhuPuli <sindhureddyp86@gmail.com>
2025-07-31 19:15:51 -0500

    First version - sort function

---
+++ Sort_Packages.py
@@ -0,0 +1,28 @@
+print('Hello')
+
+def sort(width, height, length, mass):
+    if width < 150 and height < 150 and length < 150:
+        volume = width * height * length
+        print('volume:', volume)
+        if volume < 1000000 and mass < 20:
+            print('check point0')
+            return 'NORMAL'
+        elif (volume >= 1000000 and mass < 20) or (volume < 1000000 and mass >= 20):
+            print('check point1')
+            return 'SPECIAL'
+        else:
+            print('check point2')
+            return 'REJECTED'
+    elif mass < 20:
+        print('check point3:')
+        return 'SPECIAL'
+    else:
+        print('check point4')
+        return 'REJECTED'
+    
+print('Output check point0: ', sort(20, 130, 130, 19))
+print('Output check point1: ', sort(120, 130, 130, 19))
+print('Output check point2: ', sort(120, 130, 130, 20))
+print('Output check point3: ', sort(20, 130, 150, 19))
+print('Output check point4: ', sort(20, 130, 150, 20))
+    
